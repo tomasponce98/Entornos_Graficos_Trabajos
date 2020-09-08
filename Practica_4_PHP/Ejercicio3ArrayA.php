403ac3afdb7699ad5d92a0ef4e375fceb0c08d97
Tomás Ponce <poncetomaseduardo@gmail.com>
2020-09-07 22:18:48 -0300

    Practica 4 PHP ahora si finalizada.

---
+++ Practica_4_PHP/Ejercicio3ArrayA.php
@@ -0,0 +1,1 @@
+<?php $fun = getdate(); echo "Has entrado en esta pagina a las $fun[hours] horas, con $fun[minutes] minutos y $fun[seconds] segundos, del $fun[mday]/$fun[mon]/$fun[year]"; ?>
---
+++ Practica_4_PHP/Ejercicio3ArrayB.php
@@ -0,0 +1,6 @@
+<?php function sumar($sumando1,$sumando2){
+$suma=$sumando1+$sumando2;
+echo $sumando1."+".$sumando2."=".$suma;
+}
+sumar(5,6);
+?>
